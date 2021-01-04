8960a00cae8d885a00c11fa08e0ebcc17ef469a4
krishjavaprof <64945915+krishjavaprof@users.noreply.github.com>
2021-01-04 14:40:16 +0530

    test

---
+++ src/app/models/insurance.model.ts
@@ -0,0 +1,9 @@
+import { PatientUser } from './patient.model';
+
+export class Insurance{
+     insuranceId:string;
+     policyGroupFeca:string
+     employerNameSchool: string
+     otherBenefitPlan:string
+     patient : PatientUser
+}
