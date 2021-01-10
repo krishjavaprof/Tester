ab9e582939995abd5bf581f29c173c9f789a61b4
vinod <vinod.it69@gmail.com>
2021-01-10 12:17:55 +0530

    table implimentation

--- src/app/components/login-success/provider-dashboard/appointments/appointments.component.ts
+++ src/app/components/login-success/provider-dashboard/appointments/appointments.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 
+const ELEMENT_DATA: any[] = [
+  {appointmentId: 123478, patientName: 'Sam', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '',code: 'orange'},
+  {appointmentId: 123478, patientName: 'Sam', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'green'},
+  {appointmentId: 123478, patientName: 'Sam', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'orange'},
+  {appointmentId: 123478, patientName: 'Sam', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'green'}
+];
+
 @Component({
   selector: 'app-provider-appointments',
   templateUrl: './appointments.component.html',
   styleUrls: ['./appointments.component.css']
 })
 export class AppointmentsComponent implements OnInit {
-
+  displayedColumns: string[] = ['Appointment ID', 'Patient Name', 'Date & Time', 'Reason for Visit', 'status', 'Action'];
+  dataSource = ELEMENT_DATA; 
     constructor(){}
 
     public ngOnInit() {
-                
+                    
     }
   
 
