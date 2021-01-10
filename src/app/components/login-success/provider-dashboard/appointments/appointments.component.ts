fa9336e62d5b4d2842e79b9c887dd4564f5b62aa
vinod <vinod.it69@gmail.com>
2021-01-10 19:13:38 +0530

    added filter for provider name

--- src/app/components/login-success/provider-dashboard/appointments/appointments.component.ts
+++ src/app/components/login-success/provider-dashboard/appointments/appointments.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 const ELEMENT_DATA: any[] = [
-  {appointmentId: 123478, patientName: 'Sam', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '',code: 'orange'},
-  {appointmentId: 123478, patientName: 'Sam', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'green'},
-  {appointmentId: 123478, patientName: 'Sam', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'orange'},
-  {appointmentId: 123478, patientName: 'Sam', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'green'}
+  { appointmentId: 123478, patientName: 'sam', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'orange' },
+  { appointmentId: 123478, patientName: 'john', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'green' },
+  { appointmentId: 123478, patientName: 'peter', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'orange' },
+  { appointmentId: 123478, patientName: 'david', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'green' },
+  { appointmentId: 123478, patientName: 'joel', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'red' },
+  { appointmentId: 123478, patientName: 'david', date: '12/12/2020 10:24AM', reason: 'check up Test', status: 'modified', action: '', code: 'green' }
+
+
 ];
 
 @Component({
@@ -15,16 +20,33 @@ const ELEMENT_DATA: any[] = [
 })
 export class AppointmentsComponent implements OnInit {
   displayedColumns: string[] = ['Appointment ID', 'Patient Name', 'Date & Time', 'Reason for Visit', 'status', 'Action'];
-  dataSource = ELEMENT_DATA; 
-    constructor(){}
+  dataSource = ELEMENT_DATA;
+  formGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
-    public ngOnInit() {
-                    
+  public ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.formGroup = this.formBuilder.group({
+      'providerName': [''],
+      'date': [''],
+      'status': ['']
+    });
+  }
+
+  public onSubmit(): void {
+    const pName = this.formGroup.controls.providerName.value;
+    if (pName) {
+      this.dataSource = ELEMENT_DATA.filter(data => data.patientName == pName);
+    } else {
+      this.dataSource = ELEMENT_DATA;
     }
-  
+  }
 
 }
 
 
- 
-   
+
+
